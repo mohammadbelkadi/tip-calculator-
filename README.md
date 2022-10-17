7b2e4070b09a938dcd3ba62a2785c1b05a77066d
Mohammad Belkadi <uncleruckus@mohammad.myfiosgateway.com>
2022-10-16 21:01:18 -0400

    tip cal

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mohammads Tip Calculator
+
+## User starts by entering the bill amount. After the amount is entered, the user is asked what tip percentage they would like to give. Once the tip percentage is entered, it asks how many people are splitting the bill. Finally, it generates the total and splits it amongst the number of people splitting the bill.
+
---
+++ tip_calculator.py
@@ -0,0 +1,24 @@
+
+
+print("Thanks for eating with us")
+
+# Here the user inputs the bill (tax not included at this point)
+# The input function allows the user to take the users input 
+#  we must use float because we may be dealing with decimals
+amount = float(input("Enter your bill amount ($): "))
+
+# user then inputs the tip percentage
+tip = float(input("what percentage of tip do you want to give? "))
+
+# Here the tip is calculated 
+x = ((0.01*tip)*amount)
+print("the tip will be $",x)
+
+# Here we ask the user how many people ar splitting the bill.
+split = float(input("How many people are spliting the bill: "))
+
+Y = float((amount + (0.01 * tip)* amount) + amount * .10) 
+print("The total is $",Y)
+
+tip2 = float((amount + (0.01 * tip)* amount) + amount * .10) / split
+print("The amount each person has to pay is $", tip2)
